0665f89938131719ea6ac439fc31cf4b19fb1f8f
Burak Arslan <burak.arslan@eu.sony.com>
2018-03-12 16:38:22 +0300

    [Burak] - React Workshop changes.

--- src/config/config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-	apiUrl: process.env.REACT_APP_API_URL || 'http://localhost:3001',
-	endpoints: {
-		WORKS: '/api/works/all',
-		WORK_DETAILS: '/api/work/{{item}}',
-	},
-};
-
-export default config;
--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import { routerReducer } from 'react-router-redux';
-import works from './works';
-
-export default combineReducers({
-	routing: routerReducer,
-	works,
-});

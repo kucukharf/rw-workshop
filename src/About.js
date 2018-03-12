2ce9314690c1f53eb438a07dbdb99258af34ae7c
Burak Arslan <burak.arslan@eu.sony.com>
2018-03-12 17:40:50 +0300

    [Burak] - Router integrated with home, about, works, contact, error container components.

---
+++ src/About.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+  render() {
+    return (
+        <main>
+          <div className="container fadeInUp">
+            About
+          </div>
+        </main>
+    );
+  }
+}
+
+export default About;
--- src/App.js
+++ src/App.js
@@ -1,52 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import Collapse from './Collapse';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from "react-router-dom";
+
+import Header from './Header';
+import Footer from './Footer';
+import Home from './Home';
+import About from './About';
+import Works from './Works';
+import Contact from './Contact';
+import Error from './Error';
 
 class App extends Component {
   render() {
     return (
-      <div className="Home">
-        <header>
-          <nav>
-            <section className="container">
-              <aside className="link-border">
-                <a className="active">
-                  hello world
-                </a>
-              </aside>
-              <aside className="link-border">
-                <a href="/home"> home </a>
-                <a href="/about"> about </a>
-                <a href="/works"> works </a>
-                <a href="/contact"> contact</a>
-              </aside>
-            </section>
-          </nav>
-        </header>
-        
-        <main>
-          <div className="fadeInUp">
-            <header className="container header fadeInUp">
-              <h1> Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</h1>
-              <h2> ellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium.</h2>
-              <a href="#" className="btn dark"> Get in touch </a> 
-            </header>
-          </div>
-        </main>
-        <footer>
-        <section className="container">
-         <aside className="link-border">
-                <a className="active">
-                  twitter
-                </a>
-                <a className="active">
-                  facebook
-                </a>
-            </aside>
-          </section>
-        </footer>
-      </div>
+   <div className="App">
+      <Router>
+    <div>
+      <Header/>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/home" exact component={Home} />
+        <Route path="/about" exact component={About} />
+        <Route path="/works" exact component={Works} />
+        <Route path="/contact" exact component={Contact} />
+        <Route component={Error} />
+      </Switch>
+      <Footer/>
+    </div>
+  </Router>
+    </div>
     );
   }
 }

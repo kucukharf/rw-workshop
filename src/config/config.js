8d0d97327c17007b0aa05600e1ef22b2b87bea2c
Arslan <burakarslan17@gmail.com>
2019-11-21 14:39:55 +0300

    [Burak] - configs are updated.

--- src/config/config.js
+++ src/config/config.js
@@ -1,11 +1,11 @@
-const base = process.env.REACT_APP_ENV === 'development' ? 'http://localhost:3001' : 'api.burakarslan.com.tr';
+const base = process.env.REACT_APP_ENV === 'development' ? 'http://localhost:3001' : 'http://api.burakarslan.com.tr';
 
 const config = {
 	env:process.env.REACT_APP_ENV || 'development',
 	API_URL: process.env.REACT_APP_API_URL || base,
 	MEDIA_URL: process.env.REACT_APP_API_URL || base + '/netlify/functions/media',
 	endpoints: {
-		WORKS: '/netlify/functions/works',
+		WORKS: '/.netlify/functions/works',
 		WORK_DETAILS: '',
 	},
 	domain:{
